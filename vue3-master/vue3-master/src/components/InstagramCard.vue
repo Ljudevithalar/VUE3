96e25c8ad946da2dcb9ba77d71a21bcbf9060ee7
Ljudevithalar <58052160+Ljudevithalar@users.noreply.github.com>
2019-12-12 22:55:24 +0100

    Add files via upload

---
+++ vue3-master/vue3-master/src/components/InstagramCard.vue
@@ -0,0 +1,25 @@
+<template>
+
+      <div class="card text-center">
+        <div class="card-header text-left">
+          {{info.title}}
+        </div>
+        <div class="card-body">
+          <img class="card-img-top" :src="info.url" alt="Slikasada">
+        </div>
+        <div class="card-footer text-left">
+          <div class="row">
+        <div class="col-sm-6">
+          {{info.time}}
+        </div>
+        <div class="col-sm-6">
+          {{info.id}}
+        </div>
+      </div>
+      </div></div>
+</template>
+<script>
+export default {
+ props: ["info"]
+}
+</script>
---
+++ vue3-master/vue3-master/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row">
+    <div class="col-8">
+
+      <InstagramCard :key="a" :info="a" v-for="a in trazi"/>
+
+    </div>
+    <div class="col-4">
+      Kao neki sidebar
+    </div>
+  </div>
+</template>
+
+<script>
+import InstagramCard from '@/components/InstagramCard.vue'
+import store from '@/store.js'
+export default {
+   data() {
+ return {
+ cards: [
+ { id:'bice bolje',title: 'jedan', time: 'an hour ago', url: 'https://picsum.photos/id/1012/400/400' },  
+ { id:'drugi puta',title: 'dva', time: '2 days ago', url: 'https://picsum.photos/id/1011/400/400' }, 
+  { id:'nismo se snasli',title: 'tri', time: '3 days ago', url: "https://picsum.photos/id/1013/400/400" }, 
+ ]
+ }
+ },
+ computed:{
+   trazi(){
+   return this.cards.filter(nesto=>nesto.title.includes(store.searchTerm)||nesto.id.includes(store.searchTerm))
+   }
+ },
+  name: 'home',
+  components: {
+    InstagramCard
+  }
+}
+</script>
+
+<style lang="scss">
+
+.card-body {
+  padding: 0px;
+}
+
+</style>
